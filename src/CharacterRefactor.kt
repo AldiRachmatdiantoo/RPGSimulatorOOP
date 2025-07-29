57a89833737b22fc7850aac93484736258349011
AldiRachmatdiantoo <aldirachmatdianto7@gmail.com>
2025-07-29 14:35:59 +0700

    commit commat cammit

---
+++ src/CharacterRefactor.kt
@@ -0,0 +1,26 @@
+class CharacterRefactor {
+    var health: Int = 100
+    var damage: Int = 10
+    var level: Int = 1
+    fun createChar(username: String, type: String, weapon: String) : Character{
+        when (type) {
+            "strength" -> {
+                when (weapon) {
+                    "swords" -> damage += 50
+                    "bow" -> damage += 40
+                    "shield" -> health += 60
+                }
+            }
+            "magic" -> {
+                when (weapon) {
+                    "wand" -> damage += 60
+                    "magic ring" -> damage += 40
+                    "gauntlet" -> {
+                        health += 60
+                    }
+                }
+            }
+        }
+        return Player(username, health, level, damage, weapon, type)
+    }
+}
--- src/MakeChar.kt
+++ src/MakeChar.kt
@@ -2,55 +2,46 @@ class MakeChar() {
     lateinit var username: String
     lateinit var type: String
     lateinit var weapon: String
-    var health: Int? = 100
-    var level: Int? = 1
-    var damage: Int? = 10
+    lateinit var listWeapon: MutableList<String>
+
 
     fun start(){
         println("=====BUAT CHARACTER=====")
         username = checkNullOrBlank("Username: ")
         println("Berhasil membuat username!")
         makeTypeChar()
+
     }
     fun makeTypeChar() {
         val typeChar = mutableListOf("strength", "magic")
             type = checkFilterList("Pilih Type: ", typeChar)
+            when(type){
+                "strength" -> {
+                    listWeapon = mutableListOf("swords", "bow", "shield")
+                }
+                "magic" -> {
+                    listWeapon = mutableListOf("wand", "magic ring", "gauntlet")
+                }
+            }
             println("Berhasil Memilih Type!")
-            makeWeaponChar(type)
+            makeWeaponChar()
     }
-    fun makeWeaponChar(type: String?) {
-        when(type){
-            "strength" -> {
-                val strengthWeapon = mutableListOf("swords", "bow", "shield")
-                weapon = checkFilterList("Pilih Weapon: ", strengthWeapon)
-                when(weapon) {
-                    "swords" -> {damage = (damage ?: 0) + 50 }
-                    "bow" -> {damage = (damage ?: 0) + 40}
-                    "shield" -> {health = (health ?: 0) + 60}
-                }
-            }
-            "magic" -> {
-                val magicWeapon = mutableListOf("wand", "magic ring", "gauntlet")
-                weapon = checkFilterList("Pilih Weapon: ", magicWeapon)
-                when(weapon) {
-                    "wand" -> {damage = (damage?:0) + 60}
-                    "magic ring" -> {damage = (damage?:0) + 40}
-                    "gauntlet" -> {health = (health?:0) + 60}
-                }
+    fun makeWeaponChar() {
 
-            }
-        }
+        weapon = checkFilterList("Pilih Weapon: ", listWeapon)
         println("Berhasil memilih Weapon!")
-        val makeSure = checkYesOrNo("Buat Character? ")
-        if (!makeSure){
-            println("Buat ulang..")
+        makeSure()
+    }
+    fun makeSure(){
+        val check = checkYesOrNo("Buat Akun? ")
+        if (!check) {
+            println("\nBuat Ulang..\n")
             start()
         } else {
-            val player  = Player(username, health, level, damage, weapon, type)
+            val player = CharacterRefactor().createChar(username, type, weapon)
             player.display()
         }
     }
-
 
 
 
